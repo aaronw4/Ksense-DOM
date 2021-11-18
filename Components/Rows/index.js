ef0a78ecbf5985fa70773a39ae5b29decb671b53
Aaron Williams <aaronrw4@yahoo.com>
2021-11-17 23:14:23 -0800

    Wrote js for table rows

---
+++ Components/Rows/index.js
@@ -0,0 +1,33 @@
+let data = axios.get('https://jsonplaceholder.typicode.com/users')
+let table = document.querySelector('table')
+let body = document.createElement('tbody')
+
+data.then(result => {
+    result.data.forEach(element => {
+        let row = document.createElement('tr')
+
+        let username = document.createElement('th')
+        username.setAttribute('scope', 'row')
+        username.textContent = element.username
+
+        let name = document.createElement('td')
+        name.textContent = element.name
+
+        let company = document.createElement('td')
+        company.textContent = element.company.name
+
+        let link = document.createElement('td')
+        let anchor = document.createElement('a')
+        anchor.setAttribute('href','#')
+        anchor.textContent = 'See Posts'
+        link.appendChild(anchor)
+
+        row.appendChild(username)
+        row.appendChild(name)
+        row.appendChild(company)
+        row.appendChild(link)
+        body.appendChild(row)
+    });
+})
+
+table.appendChild(body)
